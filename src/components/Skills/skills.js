f14aacc6863096445d6e58f4301f2f45b6c04aaa
Arambh Sanoria <akshout@arambhs-air.mynetworksettings.com>
2023-11-25 11:13:45 -0500

    skill issue

--- src/components/Skills/skills.js
+++ src/components/Skills/skills.js
@@ -1,8 +1,37 @@
 import React from 'react'
-
+import './skills.css'
+import web from './web.png'
+import ui from './ui.png'
+import ios from './ios.png'
 const Skills = () => {
   return (
-    <div>Skills</div>
+      <section id='skill'>
+          <span className='title'> What I do?</span>
+          <span className='title'> Your Mom </span>
+          <div className='bars'>
+              <div className='bar'>
+                  <img src={ui} alt='Ui' className='barImg' />
+                  <div className='bartext'>
+                    <h2>UI</h2>  
+                    <p>I do this </p>
+                  </div>
+              </div>
+              <div className='bar'>
+                  <img src={web} alt='Web' className='barImg' />
+                  <div className='bartext'>
+                    <h2>Web Dev</h2>  
+                    <p>Also THis </p>
+                  </div>
+              </div>
+              <div className='bar'>
+                  <img src={ios} alt='iOS' className='barImg' />
+                  <div className='bartext'>
+                    <h2>Ios </h2>  
+                    <p>Yes this too</p>
+                  </div>
+              </div>
+          </div>
+      </section>
   )
 }
 
