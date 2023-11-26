9f63bc247bc275f6cb87c7b437785d5c92875001
Arambh Sanoria <akshout@arambhs-air.mynetworksettings.com>
2023-11-26 12:58:23 -0500

    fourms

--- src/components/ContacMe/contact.js
+++ src/components/ContacMe/contact.js
@@ -1,12 +1,17 @@
 import React from 'react'
-
+import './contact.css'
 const Contact = () => {
   return (
-      <section id='main'>
-          <from> 
-              <span className='name'> Name  </span>
+      <div id='main'>
+          <h1 className='heading'> Contact Me </h1>
+          <span className='titleDis'> Get In Touch or touch some grass</span>
+          <from className='ctfourm'> 
+              <input className='name' type='text' placeholder='Name' /> 
+              <input className='email' type='text' placeholder='Email' /> 
+              <textarea className='msg' placeholder='Message' rows={5} /> 
+              <button className='subBtn' type='submit' value='Send'> Submit </button>
           </from>
-    </section>
+    </div>
   )
 }
 
